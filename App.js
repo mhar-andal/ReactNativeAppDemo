6a3a8958082eadd0a1e160ebae20a2279f9ba648
Mhar Andal <mharandal96@gmail.com>
2018-06-28 16:05:56 -0500

    :sparkles:Adds assets, assets fetching, DrawerNavigation, and main app root:sparkles:

--- App.js
+++ App.js
@@ -1,24 +1,122 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Dimensions, Image, Text, StyleSheet, View } from 'react-native';
+import Expo, { AppLoading, Asset, Font } from 'expo';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+import { DrawerNavigator, DrawerItems } from 'react-navigation';
 
 import { Constants } from 'expo';
 import { Button } from 'react-native-elements'; // 0.19.1
 
-import '@expo/vector-icons'; // 6.3.1
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
-export default class App extends Component {
+// const CustomDrawerContentComponent = props => (
+//   <View style={{ flex: 1, backgroundColor: '#43484d' }}>
+//     <View
+//       style={{ marginTop: 40, justifyContent: 'center', alignItems: 'center' }}
+//     >
+//       <Image
+//         source={require('./src/images/logo.png')}
+//         style={{ width: SCREEN_WIDTH * 0.57 }}
+//         resizeMode="contain"
+//       />
+//     </View>
+//     <View style={{ marginLeft: 10 }}>
+//       <DrawerItems {...props} />
+//     </View>
+//   </View>
+// );
+
+const MainRoot = DrawerNavigator(
+  {
+    Login: {
+      path: '/login',
+      screen: Login,
+    },
+    Profile: {
+      path: '/profile',
+      screen: Profile,
+    },
+    Lists: {
+      path: '/lists',
+      screen: Lists,
+    },
+    Components: {
+      path: '/components',
+      screen: Components,
+    },
+    Ratings: {
+      path: '/ratings',
+      screen: Ratings,
+    },
+    Pricing: {
+      path: '/pricing',
+      screen: Pricing,
+    },
+    Settings: {
+      path: '/settings',
+      screen: Settings,
+    },
+  },
+  {
+    initialRouteName: 'Components',
+    contentOptions: {
+      activeTintColor: '#548ff7',
+      activeBackgroundColor: 'transparent',
+      inactiveTintColor: '#ffffff',
+      inactiveBackgroundColor: 'transparent',
+      labelStyle: {
+        fontSize: 15,
+        marginLeft: 0,
+      },
+    },
+    drawerWidth: SCREEN_WIDTH * 0.8,
+    contentComponent: CustomDrawerContentComponent,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+  }
+);
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+export default class AppContainer extends Component {
+  state = {
+    isReady: false,
+    authenticated: false,
+  };
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/images/bg_screen4.jpg'),
+    ]);
+
+    const fontAssets = cacheFonts([FontAwesome.font, Ionicons.font]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Button
-          raised
-          icon={{ name: 'home', size: 32 }}
-          buttonStyle={{ backgroundColor: '#ff4f00', borderRadius: 10 }}
-          textStyle={{ textAlign: 'center' }}
-          title={`Welcome to\nReact Native Elements`}
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          // onError={console.warn}
         />
-      </View>
-    );
+      );
+    }
   }
 }
 
